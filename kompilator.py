29cb393e2c505a8ba7006ba95bb108addc82e7dd
Jakub Krać <162826716+Afromentiii@users.noreply.github.com>
2025-05-27 09:48:32 +0200

    Dodawanie regexow

--- kompilator.py
+++ kompilator.py
@@ -36,6 +36,7 @@ tokens = (
    'PRAWY_NAWIAS',
 )
 
+#W REGEX ANALIZA ODBYWA SIE WZGLEDEM DLUGOSCI REGEXU, CZYLI DLUZSZE SA PIERWSZE W KOLEJNOSCi
 t_LICZBA = r'[0-9]+'
 t_ZMIENNA_CALKOWITA_SLOWO_KLUCZOWE = r'calkowita [a-zA-Z]+[0-9]*'
 
@@ -44,9 +45,9 @@ t_MINUS = r'-'
 t_RAZY = r'\*'
 t_DZIELENIE = r':'
 
-t_ROWNE = r' ='
+t_ROWNE = r'='
 t_WIEKSZE = r'>'
-t_WIEKSZE_ROWNE = r'>='
+t_WIEKSZE_ROWNE = r'>= '
 t_MNIEJSZE = r'<'
 t_MNIEJSZE_ROWNE = r'<='
 
@@ -64,7 +65,7 @@ t_PETLA_DOPOKI_SLOWO_KLUCZOWE = r'dopoki'
 t_LEWY_NAWIAS = r'\('
 t_PRAWY_NAWIAS = r'\)'
 
-data = ''' ddoraz oraz lub nie nienie < > ==>>'''
+data = ''' ='''
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
