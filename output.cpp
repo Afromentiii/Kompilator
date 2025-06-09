9bee5c205663770d9442d123dc72601f3ceeb0e0
Jakub Krać <jakubkrac0123@gmail.com>
2025-06-09 16:23:06 +0200

    Naprawienie bledu ze zlym wklejaniem lub.

--- output.cpp
+++ output.cpp
@@ -1,17 +1,10 @@
 #include <iostream> 
 using namespace std; 
 int main() { 
-int a = 5;
-cout << a << endl;
-a = 5;
-int b = 11+22-33+11/5*3;
-cout << b << endl;
-int c = a;
-cout << c << endl;
-c = b+5+c+a+100;
-cout << c << endl;
-cout << a << endl;
-if (a) {
+int a = 1;
+int b = 1;
+if (a||b) {
+cout << a << " " << b << endl;
 }
 return 0;
 }
